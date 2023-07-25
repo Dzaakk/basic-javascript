28a2acb014c04a9566f826c774b16d5caeab6770
Dzkzzz <dzdzaakk@gmail.com>
2023-07-25 23:54:53 +0700

    class inheritance

---
+++ oop/class-inheritance.js
@@ -0,0 +1,23 @@
+class Employee {
+  sayHello(name) {
+    console.info(`Hello ${name}, my name is employee ${this.name}`);
+  }
+}
+
+//manager will have anything that employee have
+class Manager extends Employee {
+  sayHello(name) {
+    console.info(`Hello ${name}, my name is manager ${this.name}`);
+  }
+}
+
+const agentA = new Employee();
+agentA.name = "Agent A";
+agentA.sayHello("agent B");
+
+const agentB = new Employee();
+agentB.name = "Agent B";
+agentB.sayHello("agent A");
+
+console.info(agentA);
+console.info(agentB);
