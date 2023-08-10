92f4b43e9bbc0dbe58e95534491bdaf4595ffc2a
Dzkzzz <dzdzaakk@gmail.com>
2023-08-11 01:52:23 +0700

    math

---
+++ library/math.js
@@ -0,0 +1,9 @@
+{
+  // Math
+  console.info(Math.max(10, 12, 14, 16));
+  console.info(Math.min(10, 12, 14, 16));
+  console.info(Math.round(10.5));
+  console.info(Math.round(10.3));
+  console.info(Math.floor(10.8));
+  console.info(Math.ceil(10.3));
+}
