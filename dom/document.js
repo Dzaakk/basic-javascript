0a406a5434c6acf64513f323cfd44c1766210667
Dzkzzz <dzdzaakk@gmail.com>
2023-08-13 22:22:20 +0700

    document

---
+++ dom/document.js
@@ -0,0 +1,16 @@
+console.info(document);
+
+// document property
+console.info(document.title);
+console.info(document.head);
+console.info(document.body);
+console.info(document.images);
+console.info(document.contentType);
+
+// document method
+{
+  <p id="tulisan">Tulisan</p>;
+
+  const tulisan = document.getElementById("tulisan");
+  console.info(tulisan);
+}
